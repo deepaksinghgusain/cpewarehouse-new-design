5a7c9dbce4d6059ed0dd774ca915516f68074ef8
Deepak Singh Gusain <99547723+deepaksinghgusain@users.noreply.github.com>
2026-04-23 12:59:41 +0530

    login page

--- app/(guest)/login/page.tsx
+++ app/(guest)/login/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const LoginPage = async () => {
 
-    const res = await getPageContent('login');
+    const res = await getPageContent('login-page');
 
     let heroImageSection: any;
 
